e3a3ec0757e111b3af924dfcad106f05a59a151a
Matthew Sacks <matthew@matthewsacks.com>
2012-10-22 14:51:51 -0700

    broken: find key using dict.has_key()

--- SALT-ASSIGN.py
+++ SALT-ASSIGN.py
@@ -7,7 +7,7 @@ import StringIO
 def getInstanceId():
     curlObj = pycurl.Curl()
     curlObj.setopt(pycurl.URL, "http://localhost/") #This index.html should return an arbitrary string for instanceId
-#    curlObj.setopt(pycurl.URL, "http://169.254.169.254/latest/meta-data/instance-id") #production use
+    #    curlObj.setopt(pycurl.URL, "http://169.254.169.254/latest/meta-data/instance-id") #production use
     buffer = StringIO.StringIO()
     curlObj.setopt(pycurl.WRITEFUNCTION, buffer.write)
     curlObj.setopt(pycurl.FOLLOWLOCATION, 1)
@@ -22,14 +22,12 @@ def getInstanceMeta():
     # Using yaml.load only returns the last document.
     # This returns a generator type object instead of a dict, the Python docs
     # don't really describe how to work with this
-    myDict = yaml.load_all(stream)
-    print myDict
-
+    myDict = yaml.load(stream)
 
     #the following does not work
-    for key in myDict:
-        if key in myDict:
-            print key, myDict[key] #just get this to print for now
+    if key in myDict.has_key('instanceId') == True:
+        for key in myDict:
+             print key, myDict[key] #just get this to print for now
 
 
 def main():
