53af4d7ccd221781a06da310a5539c7655b599a1
Matthew Sacks <matthew@matthewsacks.com>
2012-10-22 14:27:35 -0700

    fixed issue of committing symlink

--- SALT-ASSIGN.py
+++ SALT-ASSIGN.py
@@ -1,1 +1,39 @@
-/Users/matthew.sacks/dev/site-config/SALT-ASSIGN.py
+#!/bin/python26
+
+import yaml
+import pycurl
+import StringIO
+
+def getInstanceId():
+    curlObj = pycurl.Curl()
+    curlObj.setopt(pycurl.URL, "http://localhost/") #This index.html should return an arbitrary string for instanceId
+#    curlObj.setopt(pycurl.URL, "http://169.254.169.254/latest/meta-data/instance-id") #production use
+    buffer = StringIO.StringIO()
+    curlObj.setopt(pycurl.WRITEFUNCTION, buffer.write)
+    curlObj.setopt(pycurl.FOLLOWLOCATION, 1)
+    curlObj.setopt(pycurl.MAXREDIRS, 1)
+    curlObj.perform()
+    return buffer.getvalue()
+
+def getInstanceMeta():
+    instanceId = getInstanceId()
+    stream = file('SALTCMDB.yml', 'r')
+
+    # Using yaml.load only returns the last document.
+    # This returns a generator type object instead of a dict, the Python docs
+    # don't really describe how to work with this
+    myDict = yaml.load_all(stream)
+    print myDict
+
+
+    #the following does not work
+    for key in myDict:
+        if key in myDict:
+            print key, myDict[key] #just get this to print for now
+
+
+def main():
+    getInstanceMeta()
+
+if __name__ == "__main__":
+    main()
